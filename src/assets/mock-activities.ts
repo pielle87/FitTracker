c6cad2eafd012710298f7157d2acba77cc2ba9bc
PLB <pielle87@yahoo.com>
2021-02-03 16:33:42 +0100

    add activities mock

---
+++ src/assets/mock-activities.ts
@@ -0,0 +1,14 @@
+import { Activity } from '../app/DAOs/activity';
+
+export const ACTIVITIES: Activity[] = [
+  {
+    date: new Date(2021, 2, 1),
+    type: 'stretching',
+    duration: 90
+  },
+  {
+    date: new Date(2021, 2, 2),
+    type: 'running',
+    duration: 45
+  },
+]
