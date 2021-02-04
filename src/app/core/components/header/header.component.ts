9506f7d57f9509a78267842e749e25e36fa00045
PLB <pielle87@yahoo.com>
2021-02-04 18:40:27 +0100

    first commit 'Header'

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { appRoutesName } from 'src/app/app-routing.module';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  isLoggedIn = false;
+  user = { name: 'pielle' };
+  buttonText = 'LOGIN'
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  // TODO
+  // onClick() {
+  //   if (this.isLoggedIn) {
+  //     this.buttonText = 'LOGOUT';
+  //   } else {
+  //     const currentUrl = this.router.url.substr(1);
+  //     this.buttonText = (appRoutesName.ACTIVITIES === currentUrl) ? 'ACTIVITIES' : 'LOGIN';
+  //     const targetUrl = (appRoutesName.ACTIVITIES === currentUrl) ? appRoutesName.LOGIN : appRoutesName.ACTIVITIES;
+  //     this.router.navigate([targetUrl]);
+  //   }
+  // }
 
 }
