04addf1793c057592a8d9e80c769cdc901023dd2
PLB <pielle87@yahoo.com>
2021-02-04 15:47:37 +0100

    set up routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ActivitiesPageComponent } from './features/activities-page/activities-page.component';
+import { LoginPageComponent } from './features/login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'activities', component: ActivitiesPageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: '**', redirectTo: 'activities' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
