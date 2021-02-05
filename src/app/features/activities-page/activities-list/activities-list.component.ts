37afdeb9218a53d528e43dee08cf85b650492121
PLB <pielle87@yahoo.com>
2021-02-05 10:16:31 +0100

    first commit 'ActivitiesList'

--- src/app/features/activities-page/activities-list/activities-list.component.ts
+++ src/app/features/activities-page/activities-list/activities-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Activity } from 'src/app/_models/activity';
 
 @Component({
   selector: 'app-activities-list',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./activities-list.component.css']
 })
 export class ActivitiesListComponent implements OnInit {
+  @Input() activities: Activity[];
 
   constructor() { }
 
