04c5fd5ea25d060c3cee12ce4bb3a3bbfab08b2a
PLB <pielle87@yahoo.com>
2021-02-05 11:38:32 +0100

    first commit 'ActivitiesStats'

--- src/app/features/activities-page/activities-stats/activities-stats.component.spec.ts
+++ src/app/features/activities-page/activities-stats/activities-stats.component.spec.ts
@@ -1,7 +1,52 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Activity } from 'src/app/_models/activity';
 
 import { ActivitiesStatsComponent } from './activities-stats.component';
 
+describe('ActivitiesStatsComponent', () => {
+  let componentUnderTest: ActivitiesStatsComponent;
+  let actualResult: any;
+  
+  Given(() => {
+    TestBed.configureTestingModule({
+      providers: [ActivitiesStatsComponent]
+    });
+    componentUnderTest = TestBed.inject(ActivitiesStatsComponent);
+    actualResult = undefined;
+  });
+  
+  describe('METHOD: ngOnInit', () => {
+    let expectedResult: number;
+    
+    When(() => {
+      componentUnderTest.ngOnInit();
+    });
+
+    describe('GIVEN activities THEN total duration is correct', () => {
+      Given(() => {
+        componentUnderTest.activities = createFakeActivities();
+        expectedResult = 140;
+      });
+      Then('total duration', () => {
+        actualResult = componentUnderTest.totalDuration;
+        expect(actualResult).toEqual(expectedResult);
+      });
+    });
+
+    describe('GIVEN activities THEN average duration is correct', () => {
+      Given(() => {
+        componentUnderTest.activities = createFakeActivities();
+        expectedResult = 70;
+      });
+      Then('average duration', () => {
+        actualResult = componentUnderTest.avgDuration;
+        expect(actualResult).toEqual(expectedResult);
+      });
+    });
+  });
+});
+
+// PRE-BUILT TESTS
 describe('ActivitiesStatsComponent', () => {
   let component: ActivitiesStatsComponent;
   let fixture: ComponentFixture<ActivitiesStatsComponent>;
@@ -16,6 +61,7 @@ describe('ActivitiesStatsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ActivitiesStatsComponent);
     component = fixture.componentInstance;
+    component.activities = createFakeActivities();
     fixture.detectChanges();
   });
 
@@ -23,3 +69,18 @@ describe('ActivitiesStatsComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+function createFakeActivities(): Activity[] {
+  return [
+    {
+      date: new Date(2021, 2, 1),
+      type: 'stretching',
+      duration: 90,
+    },
+    {
+      date: new Date(2021, 2, 2),
+      type: 'running',
+      duration: 50,
+    },
+  ];
+}
--- src/app/features/activities-page/activities-stats/activities-stats.component.ts
+++ src/app/features/activities-page/activities-stats/activities-stats.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Activity } from 'src/app/_models/activity';
 
 @Component({
   selector: 'app-activities-stats',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./activities-stats.component.css']
 })
 export class ActivitiesStatsComponent implements OnInit {
+  @Input() activities: Activity[];
+  totalDuration: number
+  avgDuration: number
 
   constructor() { }
 
   ngOnInit(): void {
+    this.totalDuration = this.activities.reduce((total, activity) => total += activity.duration, 0);
+    this.avgDuration = this.totalDuration / this.activities.length;
   }
 
 }
