74426f8096df458d93c61330600230afc463632e
PLB <pielle87@yahoo.com>
2021-02-06 19:15:25 +0100

    first commit 'Login' (formGroup)

--- src/app/features/login-page/login-page.component.ts
+++ src/app/features/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
 
   constructor() { }
 
