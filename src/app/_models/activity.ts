d8d8eeef2c79267c20657c832a82b62ebe735422
PLB <pielle87@yahoo.com>
2021-02-07 12:49:01 +0100

    refactor activity model (enum)

--- src/app/_models/activity.ts
+++ src/app/_models/activity.ts
@@ -2,15 +2,18 @@ import { Url } from "url";
 
 export interface Activity {
   id?: number // TODO: remove the optional. I may need to call it with activity: Omit<Activity, 'id'> or a Partial<>
-  date: Date;
+  date?: Date; // TODO: remove the optional
   type: string;
   duration: number;
   notes?: string; // TODO: remove the optional
-  feelingColor?: EnumColor; // TODO: remove the optional
+  feelingColor?: FeelingColors; // TODO: remove the optional
   feeling?: string; // TODO: remove the optional
   link?: Url; // TODO: remove the optional
 }
 
-interface EnumColor {
-  // TODO: fill in the colors I need
+export enum FeelingColors {
+  green = 'green',
+  yellow = 'yellow',
+  orange = 'orange',
+  red = 'red',
 }
