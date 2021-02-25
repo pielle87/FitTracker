62e5e4c6b32826f12676fc1ee502ccd10db9a41f
PLB <pielle87@yahoo.com>
2021-02-25 11:18:27 +0100

    improve tests for ActivitiesListComponent

--- src/app/features/activities-page/activities-list/activities-list.component.spec.ts
+++ src/app/features/activities-page/activities-list/activities-list.component.spec.ts
@@ -2,6 +2,41 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ActivitiesListComponent } from './activities-list.component';
 
+import { ObserverSpy, subscribeSpyTo } from '@hirez_io/observer-spy';
+import { Activity, FeelingColors } from 'src/app/_models/activity';
+
+describe('ActivitiesListComponent', () => {
+  let componentUnderTest: ActivitiesListComponent;
+  let idToDeleteSpy: ObserverSpy<any>;
+
+  Given(() => {
+    TestBed.configureTestingModule({
+      providers: [ActivitiesListComponent]
+    });
+
+    componentUnderTest = TestBed.inject(ActivitiesListComponent);
+
+  });
+
+  describe('METHOD onDelete: output idToDelete', () => {
+    let fakeActivity: Activity = createFakeActivity();
+
+    When(() => {
+      componentUnderTest.onDelete(fakeActivity);
+    });
+
+    describe('GIVEN an activity to delete', () => {
+      Given(() => {
+        idToDeleteSpy = subscribeSpyTo(componentUnderTest.idToDelete);
+      });
+      Then('Delete the activity', () => {
+        expect(idToDeleteSpy.getFirstValue()).toEqual(fakeActivity.id);
+      });
+    });
+  });
+});
+
+// PRE-BUILT TESTS
 describe('ActivitiesListComponent', () => {
   let component: ActivitiesListComponent;
   let fixture: ComponentFixture<ActivitiesListComponent>;
@@ -22,4 +57,26 @@ describe('ActivitiesListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should Output an idToDelete', () => {
+    let fakeActivity = createFakeActivity();
+    // I found this approach in the Angular doc (https://angular.io/guide/testing-components-basics#component-class-testing)
+    component.idToDelete.subscribe((id: number) => {
+      expect(id).toBe(fakeActivity.id);
+    });
+    component.onDelete(fakeActivity);
+  });
 });
+
+function createFakeActivity(): Activity {
+  return {
+    id: 1,
+    date: new Date(2021, 2, 1),
+    type: 'stretching',
+    duration: 90,
+    notes: 'HyPro',
+    feelingColor: FeelingColors.green,
+    feeling: 'good',
+    link: new URL('https://www.google.com'),
+  };
+}
